2250c5582e51f1fca86b658af284f4df3f9789ac
Zac <hendersonzacdev@gmail.com>
2022-09-19 09:40:37 +1000

    Add some test images

---
+++ segment_and_classify/shapes.m
@@ -0,0 +1,14 @@
+function [triangles, squares, circles, mask] = shapes(img)
+    % Return the centroids of all triangles, squares, and circles.
+    %
+    % Inputs Variable:
+    %        img will be a grayscale image of type uint8.
+    %
+    % Outputs:
+    %   triangles, squares, cirles should be of shape [n, 2]
+    %   mask should be the binarized image, but is not evaluated. It can be
+    %   helpful for your debugging.
+   
+    
+              
+end
