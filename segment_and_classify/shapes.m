028c0daaf0f0e907b057b31b35813f1660c69018
chengyu522 <87927986+chengyu522@users.noreply.github.com>
2022-09-19 11:44:40 +1000

    Add files via upload
    Completed shape.m

--- segment_and_classify/shapes.m
+++ segment_and_classify/shapes.m
@@ -8,37 +8,21 @@ function [triangles, squares, circles, mask] = shapes(img)
     %   triangles, squares, cirles should be of shape [n, 2]
     %   mask should be the binarized image, but is not evaluated. It can be
     %   helpful for your debugging.
-   
-    img_thres_adap = imbinarize(img, 'adaptive', 'Sensitivity', 0.62);
-    %figure;
-    %imshow(img_thres_adap);
-    
-    % Close up the holes in the image:
-    img_seg = imopen(img_thres_adap, strel('disk', 20));
-    %figure;
-    %imshow(img_seg);
-    
-    img_seg_inv = ~img_seg;
-    %figure;
-    %imshow(img_seg_inv);
-    
-    % Extract features (regions):
-    s = regionprops(img_seg_inv, 'Centroid', 'Circularity');
-    centroids = cat(1, s.Centroid);
-    
-    figure;
-    imshow(img);
-    hold on;
-    plot(centroids(:,1), centroids(:,2), 'rx');
-    hold off;
-    
-    % Classify the shapes using circularity:
-    % Note 13 circles, 4 squares and 4 triangles - 21 blobs!
-    
-    % Sort by circularity, in descending order (1 to 0).
-    % The first 13 blobs should be the circles.
-    
-    %for i = 1:21
-        
-    %end
+
+img_thres_adap = imbinarize(img, 'adaptive', 'Sensitivity', 0.65);
+
+mask = ~img_thres_adap;
+
+s = regionprops(mask, 'Centroid', 'Circularity');
+centroids = cat(1, s.Centroid);
+circularity = [s.Circularity];
+[sorted_circularity, i]= sort(circularity);
+sorted_centroids = centroids(i,:);
+triangles = sorted_centroids(1:4,:);
+squares = sorted_centroids(5:8,:);
+circles = sorted_centroids(9:21,:);
+
+
+
+              
 end
